628b99e9ac37ef73294b16eae990a1b8de63cdbd
Fabrice Gomes <fabrice.gomes@wishow.io>
2023-07-25 15:36:14 +0200

    Clean ID in routes and add md5 hash for filenames

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 from data_functions import *
 from mock_data.user_score import *
 import os
+import hashlib
 
 
 app = Flask(__name__, template_folder='templates')
@@ -19,27 +20,24 @@ def home():
     return 'hello from server'
 
 
-@app.route('/get_zip_by_user/<int:id>', methods=['POST'])
-def get_zip_by_user(id):
+@app.route('/get_zip_by_user', methods=['POST'])
+def get_zip_by_user():
     data_dict = request.json
+    id = hashlib.md5(request.data).hexdigest()
     return from_dict_to_zipfile(data_dict, id)
 
 
-@app.route('/get_radar_chart_image/<int:id>/', methods=['POST'])
-def get_radar_chart_image(id):
+@app.route('/get_radar_chart_image', methods=['POST'])
+def get_radar_chart_image():
     data_dict = request.json
+    id = hashlib.md5(request.data).hexdigest()
     return from_dict_to_radar_chart_displayed(data_dict, id)
 
 
-@app.route('/get_best_profile/', methods=['POST'])
+@app.route('/get_best_profile', methods=['POST'])
 def get_best_profile():
     dict = request.json
     return best_profile(dict)
-
-
-@app.route('/download/<int:id>/<path:filename>')
-def download(filename, id):
-    return download_file(id, filename)
 
 
 if __name__ == '__main__':
